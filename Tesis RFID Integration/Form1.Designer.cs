67c72f379707d976a134701f3bdbaedc7542d98d
Camilo Santander <camilo.santander1991@gmail.com>
2023-11-27 19:58:15 -0300

    Ya se conecta, falta lograr obtener datos de las lecturas

--- Tesis RFID Integration/Form1.Designer.cs
+++ Tesis RFID Integration/Form1.Designer.cs
@@ -28,44 +28,156 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            lblTest = new Label();
+            components = new System.ComponentModel.Container();
+            btnScanUSB = new Button();
+            grpBoxConnection = new GroupBox();
+            btnConnect = new Button();
+            lblUSB = new Label();
+            cboBoxUSB = new ComboBox();
+            textBox1 = new TextBox();
+            grpBoxLecturas = new GroupBox();
+            btnReadOnce = new Button();
+            btnStopRead = new Button();
+            btnStartRead = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            grpBoxConnection.SuspendLayout();
+            grpBoxLecturas.SuspendLayout();
             SuspendLayout();
             // 
-            // button1
+            // btnScanUSB
             // 
-            button1.Location = new Point(12, 12);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            btnScanUSB.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            btnScanUSB.Location = new Point(6, 21);
+            btnScanUSB.Name = "btnScanUSB";
+            btnScanUSB.Size = new Size(188, 23);
+            btnScanUSB.TabIndex = 0;
+            btnScanUSB.Text = "Scan USB Antenna";
+            btnScanUSB.UseVisualStyleBackColor = true;
+            btnScanUSB.Click += btnScanUSB_Click;
             // 
-            // lblTest
+            // grpBoxConnection
             // 
-            lblTest.AutoSize = true;
-            lblTest.Location = new Point(114, 16);
-            lblTest.Name = "lblTest";
-            lblTest.Size = new Size(38, 15);
-            lblTest.TabIndex = 1;
-            lblTest.Text = "label1";
+            grpBoxConnection.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            grpBoxConnection.Controls.Add(btnConnect);
+            grpBoxConnection.Controls.Add(lblUSB);
+            grpBoxConnection.Controls.Add(cboBoxUSB);
+            grpBoxConnection.Controls.Add(btnScanUSB);
+            grpBoxConnection.Location = new Point(12, 12);
+            grpBoxConnection.Name = "grpBoxConnection";
+            grpBoxConnection.Size = new Size(200, 149);
+            grpBoxConnection.TabIndex = 2;
+            grpBoxConnection.TabStop = false;
+            grpBoxConnection.Text = "Conexión";
+            // 
+            // btnConnect
+            // 
+            btnConnect.Location = new Point(6, 80);
+            btnConnect.Name = "btnConnect";
+            btnConnect.Size = new Size(188, 23);
+            btnConnect.TabIndex = 2;
+            btnConnect.Text = "Conectar";
+            btnConnect.UseVisualStyleBackColor = true;
+            btnConnect.Click += btnConnect_Click;
+            // 
+            // lblUSB
+            // 
+            lblUSB.AutoSize = true;
+            lblUSB.Location = new Point(6, 54);
+            lblUSB.Name = "lblUSB";
+            lblUSB.Size = new Size(34, 15);
+            lblUSB.TabIndex = 1;
+            lblUSB.Text = "USB: ";
+            // 
+            // cboBoxUSB
+            // 
+            cboBoxUSB.FormattingEnabled = true;
+            cboBoxUSB.Location = new Point(46, 51);
+            cboBoxUSB.Name = "cboBoxUSB";
+            cboBoxUSB.Size = new Size(148, 23);
+            cboBoxUSB.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 295);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(528, 143);
+            textBox1.TabIndex = 3;
+            // 
+            // grpBoxLecturas
+            // 
+            grpBoxLecturas.Controls.Add(btnReadOnce);
+            grpBoxLecturas.Controls.Add(btnStopRead);
+            grpBoxLecturas.Controls.Add(btnStartRead);
+            grpBoxLecturas.Location = new Point(218, 12);
+            grpBoxLecturas.Name = "grpBoxLecturas";
+            grpBoxLecturas.Size = new Size(322, 149);
+            grpBoxLecturas.TabIndex = 4;
+            grpBoxLecturas.TabStop = false;
+            grpBoxLecturas.Text = "Lecturas";
+            // 
+            // btnReadOnce
+            // 
+            btnReadOnce.Location = new Point(6, 51);
+            btnReadOnce.Name = "btnReadOnce";
+            btnReadOnce.Size = new Size(181, 23);
+            btnReadOnce.TabIndex = 2;
+            btnReadOnce.Text = "Lectura Unica";
+            btnReadOnce.UseVisualStyleBackColor = true;
+            // 
+            // btnStopRead
+            // 
+            btnStopRead.Location = new Point(112, 21);
+            btnStopRead.Name = "btnStopRead";
+            btnStopRead.Size = new Size(75, 23);
+            btnStopRead.TabIndex = 1;
+            btnStopRead.Text = "Parar";
+            btnStopRead.UseVisualStyleBackColor = true;
+            btnStopRead.Click += btnStopRead_Click;
+            // 
+            // btnStartRead
+            // 
+            btnStartRead.Location = new Point(6, 21);
+            btnStartRead.Name = "btnStartRead";
+            btnStartRead.Size = new Size(75, 23);
+            btnStartRead.TabIndex = 0;
+            btnStartRead.Text = "Iniciar";
+            btnStartRead.UseVisualStyleBackColor = true;
+            btnStartRead.Click += btnStartRead_Click;
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(lblTest);
-            Controls.Add(button1);
+            ClientSize = new Size(552, 450);
+            Controls.Add(grpBoxLecturas);
+            Controls.Add(textBox1);
+            Controls.Add(grpBoxConnection);
             Name = "Form1";
             Text = "MainForm";
+            grpBoxConnection.ResumeLayout(false);
+            grpBoxConnection.PerformLayout();
+            grpBoxLecturas.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
-        private Label lblTest;
+        private Button btnScanUSB;
+        private GroupBox grpBoxConnection;
+        private Label lblUSB;
+        private ComboBox cboBoxUSB;
+        private Button btnConnect;
+        private TextBox textBox1;
+        private GroupBox grpBoxLecturas;
+        private Button btnReadOnce;
+        private Button btnStopRead;
+        private Button btnStartRead;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- Tesis RFID Integration/Form1.cs
+++ Tesis RFID Integration/Form1.cs
@@ -1,4 +1,5 @@
-using RFID;
+﻿using RFID;
+using System;
 
 namespace Tesis_RFID_Integration
 {
@@ -13,23 +14,218 @@ namespace Tesis_RFID_Integration
             //Console.WriteLine("inicio");
             System.Diagnostics.Debug.WriteLine("inicio");
 
+
             string strSN = "";
             byte[] arrBuffer = new byte[512];
             int iHidNumber = 0;
             UInt16 iIndex = 0;
-            //comboBox1.Items.Clear();
+            cboBoxUSB.Items.Clear();
             iHidNumber = CFHidApi.CFHid_GetUsbCount();
             for (iIndex = 0; iIndex < iHidNumber; iIndex++)
             {
-                RFID.CFHidApi.CFHid_GetUsbInfo(iIndex, arrBuffer);
+                CFHidApi.CFHid_GetUsbInfo(iIndex, arrBuffer);
                 strSN = System.Text.Encoding.Default.GetString(arrBuffer);
 
                 System.Diagnostics.Debug.WriteLine(strSN);
 
-                //comboBox1.Items.Add(strSN);
+                cboBoxUSB.Items.Add(strSN);
             }
-            //if (iHidNumber > 0)
-            //    comboBox1.SelectedIndex = 0;
+            if (iHidNumber > 0)
+                cboBoxUSB.SelectedIndex = 0;
         }
+
+        private void btnScanUSB_Click(object sender, EventArgs e)
+        {
+            string strSN = "";
+            byte[] arrBuffer = new byte[256];
+            int iHidNumber = 0;
+            UInt16 iIndex = 0;
+            cboBoxUSB.Items.Clear();
+            iHidNumber = CFHidApi.CFHid_GetUsbCount();
+            for (iIndex = 0; iIndex < iHidNumber; iIndex++)
+            {
+                CFHidApi.CFHid_GetUsbInfo(iIndex, arrBuffer);
+                strSN = System.Text.Encoding.Default.GetString(arrBuffer);
+                cboBoxUSB.Items.Add(strSN);
+            }
+            if (iHidNumber > 0)
+                cboBoxUSB.SelectedIndex = 0;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            string btnConnectText = btnConnect.Text;
+
+            if (btnConnect.Text.Equals("Conectar"))
+            {
+                byte[] arrBuffer = new byte[64];
+
+                if (RFID.CFHidApi.CFHid_OpenDevice((UInt16)cboBoxUSB.SelectedIndex))
+                {
+                    if (RFID.CFHidApi.CFHid_GetDeviceSystemInfo(0xFF, arrBuffer) == false) //获取系统信息失败
+                    {
+                        SetText("No se pudo obtener la informacion del dispositivo\r\n");
+                        CFHidApi.CFHid_CloseDevice();
+                        return;
+                    }
+                    SetText("Conección Exitosa\r\n");
+                    btnConnect.Text = "Desconectar";
+                }
+                else
+                {
+                    this.SetText("Coneccion Fallida\r\n");
+                    return;
+                }
+                System.Diagnostics.Debug.WriteLine("Entra al IF");
+
+                string str = "", str1 = "";
+                str = String.Format("SoftVer:{0:D}.{0:D}\r\n", arrBuffer[0] >> 4, arrBuffer[0] & 0x0F);
+                this.SetText(str);
+                str = String.Format("HardVer:{0:D}.{0:D}\r\n", arrBuffer[1] >> 4, arrBuffer[1] & 0x0F);
+                this.SetText(str);
+                str = "SN:";
+                for (int i = 0; i < 7; i++)
+                {
+                    str1 = String.Format("{0:X2}", arrBuffer[2 + i]);
+                    str = str + str1;
+                }
+                str = str + "\r\n";
+                this.SetText(str);
+                //button1.Enabled = false;
+                //button2.Enabled = true;
+
+            }
+            else if (btnConnect.Text.Equals("Desconectar"))
+            {
+                //timer1.Enabled = false;
+                CFHidApi.CFHid_CloseDevice();
+                //button1.Enabled = true;
+                //button2.Enabled = false;
+                //button6.Enabled = true;
+                //button11.Enabled = false;
+                this.SetText("Conección finalizada\r\n");
+                btnConnect.Text = "Conectar";
+            }
+
+
+
+        }
+
+        delegate void SetTextCallback(string text);
+        private void SetText(string text)
+        {
+            if (this.textBox1.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                if (this.textBox1.TextLength > 1000) this.textBox1.Text = "";
+                textBox1.Text = this.textBox1.Text + text;
+                textBox1.SelectionStart = this.textBox1.Text.Length;
+                textBox1.ScrollToCaret();
+            }
+        }
+
+        private void btnStartRead_Click(object sender, EventArgs e)
+        {
+            SetText("Leyendo\r\n");
+            CFHidApi.CFHid_ClearTagBuf();
+
+            System.Diagnostics.Debug.WriteLine(timer1.Enabled);
+            bool readInfo;
+
+            readInfo = CFHidApi.CFHid_StartRead(0xFF);
+
+            System.Diagnostics.Debug.WriteLine(readInfo);
+            timer1.Interval = 100;
+            //timer1.Enabled = true;
+            //button6.Enabled = false;
+            //button11.Enabled = true;
+        }
+
+
+        private void btnStopRead_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+
+            CFHidApi.CFHid_StopRead(0xFF);
+
+            byte[] arrBuffer = new byte[64000];
+            int iNum = 0;
+            int iTotalLen = 0;
+            byte bRet = 0;
+
+            bRet = CFHidApi.CFHid_GetTagBuf(arrBuffer, out iTotalLen, out iNum);
+
+            System.Diagnostics.Debug.WriteLine("bRet: ", bRet);
+        }
+
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("entro al timer");
+            byte[] arrBuffer = new byte[64000];
+            int iNum = 0;
+            int iTotalLen = 0;
+            byte bRet = 0;
+
+            bRet = CFHidApi.CFHid_GetTagBuf(arrBuffer, out iTotalLen, out iNum);
+
+            System.Diagnostics.Debug.WriteLine("bRet: ", bRet);
+
+            if (bRet == 1)
+            {
+                this.SetText("DevOut");
+                return; //DevOut
+            }
+            else if (bRet == 0) {
+                System.Diagnostics.Debug.WriteLine("bRet: 0?");
+
+                return;
+            }; //No Connect
+            int iTagLength = 0;
+            int iTagNumber = 0;
+            iTagLength = iTotalLen;
+            iTagNumber = iNum;
+            if (iTagNumber == 0) return;
+            int iIndex = 0;
+            int iLength = 0;
+            byte bPackLength = 0;
+            int i = 0;
+            int iIDLen = 0;
+            for (iIndex = 0; iIndex < iTagNumber; iIndex++)
+            {
+                bPackLength = arrBuffer[iLength];
+                string str2 = "";
+                string str1 = "";
+                str1 = arrBuffer[1 + iLength + 0].ToString("X2");
+                str2 = str2 + "Type:" + str1 + " ";  //Tag Type
+                if ((arrBuffer[1 + iLength + 0] & 0x80) == 0x80)
+                {   // with TimeStamp , last 6 bytes is time
+                    iIDLen = bPackLength - 7;
+                }
+                else iIDLen = bPackLength - 1;
+
+                str1 = arrBuffer[1 + iLength + 1].ToString("X2");
+                str2 = str2 + "Ant:" + str1 + " Tag:";  //Ant
+
+                string str3 = "";
+                for (i = 2; i < iIDLen; i++)
+                {
+                    str1 = arrBuffer[1 + iLength + i].ToString("X2");
+                    str3 = str3 + str1 + " ";
+                }
+                str2 = str2 + str3;
+                str1 = arrBuffer[1 + iLength + i].ToString("X2");
+                str2 = str2 + "RSSI:" + str1 + "\r\n";  //RSSI
+                iLength = iLength + bPackLength + 1;
+                this.SetText(str2);
+            }
+        }
+
     }
 }
